e5bfbadbed3a324fa3a15e4b3aeb19c9ad7fdc66
desireewp <desiree.persson@hotmail.com>
2023-06-20 14:07:06 +0200

    dunno

--- salary-calculator/src/components/Project/Helpbar/Helpbar.tsx
+++ salary-calculator/src/components/Project/Helpbar/Helpbar.tsx
@@ -2,24 +2,23 @@ import { useState } from "react";
 import "./Helpbar.css";
 import SettingsHelpContent from "../Settings/SettingsHelpContent";
 
-interface IHeplbar{
-  showHelpbar : string;
-  setShowHelpbar: React.Dispatch<React.SetStateAction<string>>
+interface IHeplbar {
+  showHelpbar: string;
+  setShowHelpbar: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Helpbar = ({showHelpbar, setShowHelpbar} : IHeplbar) => {
-
-
-
-// ----------Event handlers-----------------
-  const handleCloseHelpbar = () => { 
-    setShowHelpbar('none')
-   }
+const Helpbar = ({ showHelpbar, setShowHelpbar }: IHeplbar) => {
+  // ----------Event handlers-----------------
+  const handleCloseHelpbar = () => {
+    setShowHelpbar("none");
+  };
 
   return (
     <div className="helpbar-container-wrapper">
-      <div className="helpbar-container" style={{display : showHelpbar}}>
-        <button className="closeBtn" onClick={handleCloseHelpbar}>X</button>
+      <div className="helpbar-container" style={{ display: showHelpbar }}>
+        <button className="closeBtn" onClick={handleCloseHelpbar}>
+          X
+        </button>
         <h2>Help</h2>
         <hr></hr>
 
